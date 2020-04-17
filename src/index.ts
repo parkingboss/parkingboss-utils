48acce4b0910f5e62dd40b6ae7cf0d9d4f805699
Chris Pfohl <christopher.pfohl@gmail.com>
2020-04-17 11:48:08 -0400

    Make sure to export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ export * as infotag from'./infotag';
 export * as qs from './qs';
 export * as urls from './urls';
 export * as image from './image';
+export * as imgx from './imgx';
+export * as file from './file';
 export * as support from './support';
 export * as gps from './gps';
 export * as uuid from './uuid';
